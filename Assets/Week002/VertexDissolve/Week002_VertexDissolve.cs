41b0cb8300d6b1b03f81e39f61a938cb2f48b4ca
gyh <gyhguanyonghua@163.com>
2021-08-07 21:35:23 +0800

    vertex dissolve

---
+++ Assets/Week002/VertexDissolve/Week002_VertexDissolve.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Week002_VertexDissolve : MonoBehaviour
+{
+    public Material mat;
+
+    [Range(0.1f, 2f)]
+    public float speed = 0.5f;
+
+    void Start()
+    {
+        Mesh omesh = GetComponent<MeshFilter>().mesh;
+        var vertices = omesh.vertices;
+        var triangles = omesh.triangles;
+        var normals = omesh.normals;
+        var uv = omesh.uv;
+
+        List<int> myTriangles = new List<int>();
+        List<Vector3> myVertices = new List<Vector3>(vertices);
+        List<Vector3> myNormals = new List<Vector3>(normals);
+        List<Vector2> myUVs = new List<Vector2>(uv);
+        HashSet<int> st = new HashSet<int>();
+        int id = vertices.Length;
+        var s = "";
+        for (int i = 0; i < triangles.Length; i++)
+        {
+            var indice = triangles[i];
+            var vertex = vertices[indice];
+            var normal = normals[indice];
+            var _uv = uv[indice];
+
+            if (st.Contains(indice))
+            {
+                myTriangles.Add(id);
+                myNormals.Add(normal);
+                myVertices.Add(vertex);
+                myUVs.Add(_uv);
+                id++;
+            } else
+            {
+                st.Add(indice);
+                myTriangles.Add(indice);
+            }
+
+            // debug
+            /*
+            if (i == 0 || i % 3 != 0)
+            {
+                s += $"{indice},";
+            }
+            else
+            {
+                s = s + $"\n{indice},";
+            }
+            */
+        }
+        // debug
+        /*
+        Debug.Log($"origin=\n{s}");
+        s = "";
+        for (int i = 0; i < myTriangles.Count; i++)
+        {
+            var indice = myTriangles[i];
+            if (i == 0 || i % 3 != 0)
+            {
+                s += $"{indice},";
+            }
+            else
+            {
+                s = s + $"-->{(i - 1) / 3}\n{indice},";
+            }
+        }
+        Debug.Log($"now=\n{s}" + "-->" + (myTriangles.Count) / 3);
+        */
+
+        Mesh myMesh = new Mesh();
+        Color[] myColors = new Color[myVertices.Count];
+        for (int i = 0; i < myTriangles.Count; i += 3)
+        {
+            var noise = Random.Range(0.1f, 0.9f);
+            Vector3 center = Vector3.zero;
+            for (int j = 0; j < 3; j ++)
+            {
+                var indice = myTriangles[i + j];
+                var vertex = myVertices[indice];
+                center += vertex;
+            }
+            center = center / 3.0f;
+            for (int j = 0; j < 3; j++)
+            {
+                var indice = myTriangles[i + j];
+                myColors[indice] = new Color(center.x, center.y, center.z, noise);
+            }
+        }
+
+        myMesh.vertices = myVertices.ToArray();
+        myMesh.triangles = myTriangles.ToArray();
+        myMesh.normals = myNormals.ToArray();
+        myMesh.uv = myUVs.ToArray();
+        myMesh.colors = myColors;
+        GetComponent<MeshFilter>().sharedMesh = myMesh;
+
+        if (!mat)
+        {
+            throw new System.Exception("not set Material");
+        }
+        mat.SetFloat("_DissolveSpeed", speed);
+    }
+}
---
+++ Assets/Week002/VertexDissolve/Week002_VertexDissolve.shader
@@ -0,0 +1,63 @@
+Shader "Unlit/Week002_VertexDissolve"
+{
+    Properties
+    {
+        _Texture("Texture", 2D) = "white" {}
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float4 color : COLOR;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _Texture;
+            float _DissolveSpeed;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                float2 uv = v.uv;
+                float4 vertex = v.vertex;
+                float4 color = v.color;
+                float3 center = color.xyz;
+                float noise = color.a;
+                float weight = saturate(_Time.y * _DissolveSpeed);
+
+                float upward = saturate(weight - noise) * pow(_DissolveSpeed, noise);
+                vertex.xyz = lerp(vertex.xyz, center, weight);
+                vertex.y += upward;
+                float4 wpos = mul(UNITY_MATRIX_M, vertex);
+
+                o.vertex = mul(UNITY_MATRIX_VP, wpos);
+                o.uv = uv;
+                return o;
+            }
+
+            float4 frag(v2f i) : SV_Target
+            {
+                return tex2D(_Texture, i.uv);
+            }
+            ENDCG
+        }
+    }
+}
